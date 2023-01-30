36aac4c149339523164cf045dd9b3a473169bd78
p_pogwizd <p.pogwizd@onet.pl>
2023-01-30 01:21:24 +0100

    add .env file

--- frontend/src/pages/Components/Products.tsx
+++ frontend/src/pages/Components/Products.tsx
@@ -6,9 +6,6 @@ interface SalesProps {
 }
 
 export const Products: FC<SalesProps> = ({}) => {
-    //todo Products: chairs, desks, shelf's, wardrobes
-    //todo Create flex area for all products where every products will have:
-    //todo id: number, name: string ,img, cost: double
     return (
         <div>
             <FurnitureList/>
